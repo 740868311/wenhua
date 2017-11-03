18a8f5a3dbf0bf6a005c93f44fc4fd5072143e25
li.sheng <li.sheng@cnmo.com>
2017-11-03 17:57:17 +0800

    hd

--- application/Portal/Controller/AdminHdController.class.php
+++ application/Portal/Controller/AdminHdController.class.php
@@ -9,44 +9,34 @@
 namespace Portal\Controller;
 use Common\Controller\AdminbaseController;
 
-class CgAdminController extends AdminbaseController {
+class AdminHdController extends AdminbaseController {
 
 	private $model;
 
 	public function __construct()
 	{
 		parent::__construct();
-		$this->model	=	M('Cg');
+		$this->model	=	M('Hd');
 	}
 
 	// 活动列表
 	public function index()
 	{
-
+		$data = $this->model->select();
+		$this->data = $data;
 		$this->display();
 	}
 
 	// 添加活动
 	public function add(){
-		// 得到所属场馆
-		$one_cg 	=	M('CgOne')->select();
-		$this->assign('one_cg', $one_cg);
 
 		// 得到所属区域
 		$area		=	M('Area')->select();
 		$this->assign('area', $area);
 
-		// 得到人数条件
-		$number		=	M('CgNumber')->select();
-		$this->assign('number', $number);
-
 		// 得到类型
-		$type		=	M('CgType')->select();
+		$type		=	M('HdType')->select();
 		$this->assign('type', $type);
-
-		// 得到说所有设施
-		$facility	=	M('CgFacility')->select();
-		$this->assign('facility', $facility);
 
 		$this->display("add");
 	}
@@ -64,20 +54,28 @@ class CgAdminController extends AdminbaseController {
 			$_POST['thumb'] = sp_asset_relative_url($_POST['smeta']['thumb']);
 			$post = $_POST;
 
-			$data['one_id']			=	(int)$post['one_id'];
+			if(strlen($post['phone']) == "11")
+			{
+				//上面部分判断长度是不是11位
+				if (!preg_match_all("/13[123569]{1}\d{8}|15[1235689]\d{8}|188\d{8}/", $post['phone'], $array)) {
+					$this->error('请输入正确的手机号');die;
+				}
+			}else{
+				$this->error('手机号长度必须是11位');die;
+			}
 			$data['area_id']		=	(int)$post['area_id'];
 			$data['name']			=	htmlspecialchars($post['name']);
-			$data['number_id']		=	(int)$post['number_id'];
+			$data['sum']			=	(int)$post['sum'];
 			$data['type_id']		=	(int)$post['type_id'];
-//			$data['address']		=	htmlspecialchars($post['address']);
-			$data['facility_ids']	=	implode(',', $post['facility']);
-			$data['acreage']		=	htmlspecialchars($post['acreage']);
-			$data['number']			=	$post['number'];
-			$data['cost']			=	(int)$post['cost'];
+			$data['address']		=	htmlspecialchars($post['address']);
+			$data['phone']			=	$post['phone'];
+			$data['date']			=	htmlspecialchars($post['date']);
+			$data['time']			=	htmlspecialchars($post['time']);
 			$data['remarks']		=	htmlspecialchars($post['remarks']);
-			$data['time_page']		=	implode(',', $post['time_page']);
-			$data['image']			=	json_encode($post['image']);
+			$data['hint']			=	htmlspecialchars($post['hint']);
+			$data['host']			=	htmlspecialchars($post['host']);
 			$data['thumb']			=	$post['thumb'];
+			$data['content']		=	htmlspecialchars_decode($post['content']);
 			$data['add_time']		= 	date('Y-m-d H:i:s', time());
 
 			$result	= $this->model->add($data);
@@ -108,25 +106,13 @@ class CgAdminController extends AdminbaseController {
 		$this->assign("image",json_decode($data['image'],true));
 		$this->data = $data;
 
-		// 得到所属场馆
-		$one_cg 	=	M('CgOne')->select();
-		$this->assign('one_cg', $one_cg);
-
 		// 得到所属区域
 		$area		=	M('Area')->select();
 		$this->assign('area', $area);
 
-		// 得到人数条件
-		$number		=	M('CgNumber')->select();
-		$this->assign('number', $number);
-
 		// 得到类型
-		$type		=	M('CgType')->select();
+		$type		=	M('HdType')->select();
 		$this->assign('type', $type);
-
-		// 得到说所有设施
-		$facility	=	M('CgFacility')->select();
-		$this->assign('facility', $facility);
 
 		$this->display();
 	}
@@ -143,24 +129,30 @@ class CgAdminController extends AdminbaseController {
 			}
 			$_POST['thumb'] = sp_asset_relative_url($_POST['thumb']);
 			$post = $_POST;
-
+			if(strlen($post['phone']) == "11")
+			{
+				//上面部分判断长度是不是11位
+				if (!preg_match_all("/13[123569]{1}\d{8}|15[1235689]\d{8}|188\d{8}/", $post['phone'], $array)) {
+					$this->error('请输入正确的手机号');die;
+				}
+			}else{
+				$this->error('手机号长度必须是11位');die;
+			}
 			$data['id']				=	$post['id'];
-			$data['one_id']			=	(int)$post['one_id'];
 			$data['area_id']		=	(int)$post['area_id'];
 			$data['name']			=	htmlspecialchars($post['name']);
-			$data['number_id']		=	(int)$post['number_id'];
-//			$data['address']		=	htmlspecialchars($post['address']);
+			$data['sum']			=	(int)$post['sum'];
 			$data['type_id']		=	(int)$post['type_id'];
-			$data['facility_ids']	=	implode(',', $post['facility']);
-			$data['acreage']		=	htmlspecialchars($post['acreage']);
-			$data['number']			=	$post['number'];
-			$data['cost']			=	(int)$post['cost'];
+			$data['address']		=	htmlspecialchars($post['address']);
+			$data['phone']			=	$post['phone'];
+			$data['date']			=	htmlspecialchars($post['date']);
+			$data['time']			=	htmlspecialchars($post['time']);
 			$data['remarks']		=	htmlspecialchars($post['remarks']);
-			$data['time_page']		=	implode(',', $post['time_page']);
-			$data['image']			=	json_encode($post['image']);
+			$data['hint']			=	htmlspecialchars($post['hint']);
+			$data['host']			=	htmlspecialchars($post['host']);
 			$data['thumb']			=	$post['thumb'];
-			$data['add_time']		= 	date('Y-m-d H:i:s', time());
-
+			$data['content']		=	htmlspecialchars_decode($post['content']);
+			
 			$result	= $this->model->save($data);
 			if ($result) {
 				$this->success("修改成功！");
